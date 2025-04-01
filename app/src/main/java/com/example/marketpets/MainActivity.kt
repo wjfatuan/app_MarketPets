17543f4b4e56d79945808c90a46726891c3c78d6
Geraldine Novoa <114543357+GeralNo@users.noreply.github.com>
2025-04-01 00:04:05 -0500

    Proyecto creado

---
+++ app/src/main/java/com/example/marketpets/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.marketpets
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_app)
+
+        val Laries_Regular = findViewById<TextView>(R.id.Laries_Regular)
+        //val Raleway_Regular = findViewById<TextView>(R.id.Raleway_Regular)
+        //val Raleway_Italic = findViewById<TextView>(R.id.Raleway_Italic)
+
+        Laries_Regular.typeface = ResourcesCompat.getFont(this, R.font.laries_regular)
+        //Raleway_Regular.typeface = ResourcesCompat.getFont(this, R.font.raleway_regular)
+        //Raleway_Italic.typeface = ResourcesCompat.getFont(this, R.font.raleway_italic)
+
+
+    }
+}
