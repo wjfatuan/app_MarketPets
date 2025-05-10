3183e668f3ca7eefbd5e19bb88e4955c6da81ea5
Erick Velandia <stmartinez07@uan.edu.co>
2025-05-10 01:18:40 -0500

    progreso

--- app/src/main/java/com/example/marketpets/Views/ui/fragments/ComidaFragment.kt
+++ app/src/main/java/com/example/marketpets/Views/ui/fragments/ComidaFragment.kt
@@ -4,19 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import com.example.marketpets.R
+import com.example.marketpets.databinding.FragmentComidaBinding
 
 class ComidaFragment : Fragment() {
+
+    private var _binding: FragmentComidaBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_comida, container, false)
+    ): View {
+        _binding = FragmentComidaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnBack.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.google.gms:google-services:4.4.1")
+    }
+}
+
 
 plugins {
 
